6a0d511f15ac3a088b40e219fe2dc0cd1b9a7306
Andreas Bernauer <andreas.bernauer@jambit.com>
2020-05-28 16:19:45 +0200

    add debug statements and fix bug for SQUAD

--- abc_blocks.py
+++ abc_blocks.py
@@ -4,27 +4,40 @@ def can_make_word(word, blocks):
         letter1, letter2 = block
         return (letter1 in letters) or (letter2 in letters)
     useful_blocks = [b for b in filter(block_has_letter, blocks)]
-    def selected_blocks(letters, blocks, tried_blocks, started_with_blocks):
+    def selected_blocks(level, letters, blocks, tried_blocks, started_with_blocks):
+        def dbg_print(msg):
+            print("  "  * level, msg)
+        dbg_print(f"{level}: {letters}, {blocks} ------ {tried_blocks} -------- {started_with_blocks}")
         if len(letters) == 0:
+            dbg_print("letters exhausted")
             return True
         elif len(blocks) == 0:
             if len(tried_blocks) == 0:
+                dbg_print("blocks and tried blocks exhausted")
                 return False
             elif set(tried_blocks) == set(started_with_blocks):
+                dbg_print("tried all blocks to no avail")
                 return False
             else:
-                return selected_blocks(letters, tried_blocks, [], tried_blocks)
+                dbg_print(f"exhausted, reset to {tried_blocks}")
+                return selected_blocks(level, letters, tried_blocks, [], tried_blocks)
         else:
             block = blocks[0]
             remaining_blocks = blocks[1:]
             first_letter = letters[0]
             remaining_letters = letters[1:]
             if first_letter in block:
-                return selected_blocks(remaining_letters, remaining_blocks, tried_blocks, started_with_blocks) or \
-                       selected_blocks(letters, remaining_blocks, tried_blocks + [block], started_with_blocks)
+                dbg_print(f"For {first_letter}, trying to select block {block}")
+                did_work = selected_blocks(level + 1, remaining_letters, remaining_blocks + tried_blocks, [], remaining_blocks + tried_blocks)
+                if did_work:
+                    return True
+                else:
+                    dbg_print(f"--did not work, trying without")
+                    return selected_blocks(level + 1, letters, remaining_blocks, tried_blocks + [block], started_with_blocks)
             else:
-                return selected_blocks(letters, remaining_blocks, tried_blocks + [block], started_with_blocks)
-    return selected_blocks(letters, useful_blocks, [], useful_blocks)
+                dbg_print(f"For {first_letter}, skipping block {block} and trying next")
+                return selected_blocks(level, letters, remaining_blocks, tried_blocks + [block], started_with_blocks)
+    return selected_blocks(0, letters, useful_blocks, [], useful_blocks)
 
 
 def main():
